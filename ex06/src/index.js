97c9becafddef4bb26557de12cd8660bc52cdd44
belmapeskic <belma.peskic@gmail.com>
2022-04-05 07:14:35 +0200

    Adding file index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function xSix(num) {
+    return num * 6
+} 
+
+var answer = xSix(5); // 30
+var answer = xSix(2); // 12
+var answer = xSix(0); // 0
+// Only change code above this line
+
+console.log(xSix(5));
+console.log(xSix(2));
+console.log(xSix(0));
+
+module.exports = xSix;
