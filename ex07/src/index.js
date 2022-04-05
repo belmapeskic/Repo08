02c8c4a6579614bd1f6bdacc8f0651c0b8f285a7
belmapeskic <belma.peskic@gmail.com>
2022-04-05 10:25:28 +0200

    Adding file index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+var myVariable = "dress";
+
+function myDress() {
+    var myVariable = "ball gown";
+    return myVariable;
+} 
+// Only change code above this line
+
+console.log(myDress());
+
+module.exports = myDress;
